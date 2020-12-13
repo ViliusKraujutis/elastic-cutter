57787286b3e73ef7add053020fd47b02a78a22ab
Vilius Kraujutis <viliusk@gmail.com>
2020-12-13 10:57:33 +0200

    Arduino: spin motor back and forth, slow and quick

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 ## Items
 - NEMA 17 Stepper motor (42shdc3025-24b)
-  Holding torque: 40 N·cm, Rated Voltage: 3.96 V	
+  Holding torque: 40 N·cm, Rated Voltage: 3.96 V
 - Stepper motor driver module (a4988)
 - ESP32
 
 ## Wiring diagram
 ![Wiring diagram](https://elfnor.com/images/A4988_circuit.jpg)
+![Wiring diagram 2](https://lastminuteengineers.com/wp-content/uploads/arduino/Wiring-Nema-17-Stepper-Motor-to-DRV8825-driver-Arduino.png)
+
+
+## Coding
+Initially followed [this tutorial](https://lastminuteengineers.com/a4988-stepper-motor-driver-arduino-tutorial/).
--- elastic-cutter/elastic-cutter.ino
+++ elastic-cutter/elastic-cutter.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
-
+// Define pin connections & motor's steps per revolution
+const int dirPin = 18;
+const int stepPin = 19;
+const int stepsPerRevolution = 200;
+const int stepTime = 5000;
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
 }
+void loop()
+{
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  // Spin motor slowly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(stepTime);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(stepTime);
+  }
+  delay(1000); // Wait a second
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin, LOW);
 
+  // Spin motor quickly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(stepTime/2);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(stepTime/2);
+  }
+  delay(1000); // Wait a second
 }
